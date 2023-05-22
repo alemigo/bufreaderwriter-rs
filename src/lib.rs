2517723e85b285daea45ec72324ff83942a78273
alemigo <alexmigon@gmail.com>
2023-05-22 17:30:16 -0400

    doc and config fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 //! The `BufReaderWriterRand<RW>` and `BufReaderWriterSeq<RW>` are convenience structs that facilitate automatic
 //! switching between buffered reading and writing from a single underlying IO instance. `BufReaderWriterRand` is
-//! for random access IO (i.e. Read + Write + Seek, such as `std::fs::File`), while `BufReaderWriterSeq` is for sequential
-//! IO (i.e. Read + Write, such as `std::net::TcpStream`).
+//! for random access IO (i.e. `Read + Write + Seek`, such as `std::fs::File`), while `BufReaderWriterSeq` is for sequential
+//! IO (i.e. `Read + Write`, such as `std::net::TcpStream`).
 //!
-//! Both structs move the underlying IO instance between a BufReader and BufWriter as needed.  However, when switching from
+//! Both structs move the underlying IO instance between a `BufReader` and `BufWriter` as needed.  However, when switching from
 //! reading to writing, `BufReaderWriterRand` discards any buffered data and seeks the underlying IO instance back to the
-//! current BufReader position, while `BufReaderWriterSeq` saves any buffered data and makes it available for subsequent
+//! current `BufReader` position, while `BufReaderWriterSeq` saves any buffered data and makes it available for subsequent
 //! reads.
 //!
 //! # Example
